abc20a9e3427b8561c48d49b48bbd25138dc1e61
Piotr Rulewicz <piotr.rulewicz@hotmail.com>
2023-09-01 23:17:21 +0200

    Updated README, added pics, new init function

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # Currency Converter
 
 ## Description
-A simple currency converter as continuation of learning HTML, CSS and Javascript. The code includes: the form with a choice of options, normalize.css and the BEM convention.
+
+A simple currency converter as continuation of learning HTML, CSS and Javascript.<br>
+The code includes: the form with a choice of options, normalize.css and the BEM convention.<br>
+In Javascript code I used **arrow functions**.
+
+## How it's working
+
+This currency converter converts PLN currency to others, like EUR, CHF, USD and JPY, according<br>
+to given exchange rates.
 
 ## Warning
-**The exchange rates used in the tool are exemplary. The calculator is for practice only ;-)**
+
+**The exchange rates used in the tool are exemplary. The calculator is for practice only**
+
+## Screenshots
+
+**Screenshot 1:** default apperance, no data entered
+
+![Screenshot 1, default apperance, no data entered](img/2_screen-currency-converter_readme.jpg)
+
+**Screenshot 2:** entered data and displayed result
+
+![Screenshot 2, entered data and displayed result](img/3_screen-currency-converter_readme.jpg)
 
 ## Demo
+
 https://piotr-rulewicz.github.io/currency-converter/
--- js/script.js
+++ js/script.js
@@ -1,39 +1,31 @@
 {
-  
-const plnToPay = document.querySelector(".js-currencyConverter__value");
-
-  const calculateResult = () => {
-
-  const currencyConverterForm = document.querySelector(".form");
-  const enteredValue = document.querySelector(".js-enteredValue");
-  const selectedCurrency = document.querySelector(".js-selectedCurrency");
-  
-  currencyConverterForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const value = enteredValue.value;
-  const currency = selectedCurrency.value;
-  const toPay = value * currency;
-
-  plnToPay.innerText = toPay.toFixed(2);
-
-});
-
-}
-
-calculateResult();
 
   const init = () => {
 
-  const resetButton = document.querySelector(".js-resetButton");
+    const currencyConverterForm = document.querySelector(".form");
+    const enteredValue = document.querySelector(".js-enteredValue");
+    const selectedCurrency = document.querySelector(".js-selectedCurrency");
+    const resetButton = document.querySelector(".js-resetButton");
+    const plnToPay = document.querySelector(".js-currencyConverter__value");
 
-  resetButton.addEventListener("click", () => {
-  plnToPay.innerText = ("0.00");
+    currencyConverterForm.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-});
+      const value = enteredValue.value;
+      const currency = selectedCurrency.value;
+      const toPay = value * currency;
 
-}
+      plnToPay.innerText = toPay.toFixed(2);
 
-init();
+    });
+
+    resetButton.addEventListener("click", () => {
+      plnToPay.innerText = "0.00";
+
+    });
+
+  };
+
+  init();
 
 }
